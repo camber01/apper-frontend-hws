61c9c48845dcd283a6e78a190ec37a95c240b458
camber01 <bernabe.cambarihan@gcash.com>
2022-04-07 12:26:23 +0800

    Added homework enhanced-login

--- HW2/enhanced-login/src/App.js
+++ HW2/enhanced-login/src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import { useState } from 'react';
+
+const LoginForm = () => {
+  const [ username, setUsername ] = useState('')
+  const [ password, setPassword ] = useState('')
+  const [ isLoggedIn, setIsLoggedIn ] = useState(false)
+
+  const login = () => {
+    if((username === 'admin') && (password === 'adminpassword')){
+      setIsLoggedIn(true)
+    }
+  }
+
+  const logout = () => {
+    setIsLoggedIn(false)
+    setUsername('')
+    setPassword('')
+  }
+
+  return (
+    <>
+      {isLoggedIn ?
+        (
+          <div>
+            <h1>Hello, {username}!</h1>
+            <button className="btn-submit" onClick={logout} > Logout </button>
+          </div> 
+        )
+      :
+        ( 
+          <div>
+            <div className="form-label">
+              <label>Username:</label>
+              <input
+                className="input-style"
+                onChange={(event) => setUsername(event.target.value)}
+                type="text"
+                value={username}
+               />
+            </div>
+
+            <div className="form-label">
+              <label>Password:</label>
+              <input
+                className="input-style"
+                onChange={(event) => setPassword(event.target.value)}
+                type="password"
+                value={password}
+               />
+            </div>
+
+            <div className="form-label">
+              <button className="btn-submit" onClick={login} > Submit </button>
+            </div>
+          </div>
+        )
+      }
+    </>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoginForm />
     </div>
   );
 }
